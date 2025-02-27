4c1ada98ad5645ffee12c84574455dbcec14e69a
Luc Baier-Reinio <lucbaierreinio@gmail.com>
2025-02-27 14:06:09 -0500

    remove fcnn

--- src/models/GrambaSequenceClassificationModel.py
+++ src/models/GrambaSequenceClassificationModel.py
@@ -7,15 +7,10 @@ class GrambaSequenceClassificationModel(nn.Module):
     def __init__(self, embedding_dim, vocab_size, embedding_weights, num_layers, window_size, pad_token_id, attention_probs_dropout_prob=0.3, ratio=2, expansion_factor=4, bidirectional=False):
         super().__init__()
         self.gramba_model = GrambaModel(embedding_dim, vocab_size, embedding_weights, num_layers, window_size, pad_token_id, attention_probs_dropout_prob, ratio, expansion_factor, bidirectional)
-        self.fc1 = nn.Linear(embedding_dim, 512)
-        self.fc2 = nn.Linear(512, 512)
-        self.fc3 = nn.Linear(512, 1)
+        self.classifier = nn.Linear(embedding_dim, 1)
 
     def forward(self, x, mask=None):
         x = self.gramba_model(x, mask) # B_S, S_L, H_D
         x = x[:, -1, :]  # Should be the CLS token
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, 0.5, training=self.training)
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.classifier(x)
         return x
